42fd43e9e602f93b6c1cba74eea0b943a2f62be5
maxim-creator <max47166978@gmail.com>
2021-03-28 17:55:22 +0300

    liquibase files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,17 @@
+import models.Post;
+import models.Region;
+import models.Writer;
 import repositories.DBRepositories.DBPostRepository;
 import repositories.DBRepositories.DBRegionRepository;
 import repositories.DBRepositories.DBWriterRepository;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        ProgramStarter starter = new ProgramStarter();
-//        starter.start();
-        DBPostRepository dbPostRepository = new DBPostRepository();
-        DBRegionRepository dbRegionRepository = new DBRegionRepository();
-        DBWriterRepository dbWriterRepository = new DBWriterRepository();
-        //System.out.println(dbPostRepository.getById(1));
-        dbPostRepository.deleteById(1);
+        ProgramStarter starter = new ProgramStarter();
+        starter.start();
     }
-
-
-
 }
